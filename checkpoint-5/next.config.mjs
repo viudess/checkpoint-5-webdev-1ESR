a49c42d096eae1aff8a27ae135535f8674feff59
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-17 20:26:39 -0300

    Ajuste de configurações

--- checkpoint-5/next.config.mjs
+++ checkpoint-5/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**', 
+      },
+    ],
+  },
+};
 
 export default nextConfig;
